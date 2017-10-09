0573a54ff19d5ece91b7ee2af2ac048b43212368
LegolasTheElves <jgaffod.itc@gmail.com>
2017-10-09 13:33:35 -0700

    Add project files.

---
+++ ShapeInheritance/RectangleTester.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShapeInheritance
+{
+
+    class RectangleTester
+    {
+        static void Main(string[] args)
+        {
+            Rectangle Rect = new Rectangle();
+            Console.Write("Enter width:");
+            int w = Convert.ToInt32(Console.ReadLine());
+            Rect.setWidth(w);
+            Console.Write("Enter width:");
+            int h = Convert.ToInt32(Console.ReadLine());
+            Rect.setHeight(h);
+
+            // Print the area of the object.
+            Console.WriteLine("Total area: {0}", Rect.getArea());
+            Console.ReadKey();
+        }
+    }
+}
